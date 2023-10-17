77a3cf68340b3ae32f26bf40566ee6d9b2240bf8
Michelle <michelledng7@gmail.com>
2023-10-17 23:52:37 +1100

    add get_weather() and weather.html

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, request
 from weather import get_current_weather
+from waitress import serve
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #homepage
 @app.route('/index')
-def index():
+def index():  #return for the route
     return render_template('index.html')
     
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+    weather_data = get_current_weather(city)
+    return render_template('weather.html', 
+                           title = weather_data["name"],
+                           status=weather_data["weather"][0]["description"].capitalize(),
+                           temp=f"{weather_data['main']['temp']:.1f}",
+                           feels_like=f"{weather_data['main']['feels_like']:.1f}")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)   
+    serve(app, host='0.0.0.0', port=8000)   
 
 
